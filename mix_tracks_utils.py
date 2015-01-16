7c3bd95e4301319ab181e4c8ccf739675fae0694
Mike iLL <mike@mzoo.org>
2015-01-16 13:54:35 -0600

    More general development.

--- mix_tracks_utils.py
+++ mix_tracks_utils.py
@@ -14,6 +14,8 @@ By Mike iLL/mZoo.org, 2015-01-13.
 """
 from __future__ import print_function
 import echonest.remix.audio as audio
+from Queue import Queue
+import glob
 import sys, os
 import pickle
 
@@ -23,6 +25,70 @@ Usage:
 
 """
 
+def make_save_one(filename):
+    """
+    Get filename, make LocalAudioFile objects and save it. Return LAF.
+    """
+    audiofile = audio.LocalAudioFile(filename)
+    audiofile2 = audiofile
+    audiofile.save()
+    return audiofile2
+    
+def make_save_all(files):
+    """
+    Get a list of files, make LocalAudioFile objects and save them.
+    """
+    q = file_queue(files)
+
+    while not q.empty():
+        file = q.get()
+        save_me(file)
+        
+def file_queue(files):
+    """
+    Get list of files, add them to a queue and return the queue.
+    """
+    q = Queue() 
+    
+    for f in files:
+        q.put(f)
+    
+    return q
+    
+def get_files(directory = 'audio/', extension = 'mp3'):
+    return glob.glob(directory + '*.' + extension)
+    
+def import_queue(q):
+    """
+    Get a queue, iterate and yield a list of two imported LocalAudioFiles
+    """
+    t1 = q.get()
+    q.task_done()
+    while not q.empty():
+        t2 = q.get()
+        q.task_done()
+        audiofile1 = lazarus(t1)
+        audiofile2 = lazarus(t2)
+        yield (audiofile1, audiofile2)
+        t1 = t2
+        
+def lazarus(filename):
+    """
+    Get pickled (*.en) filename (path) and return echonest analysis object
+    """
+    with open(filename) as f:
+        return pickle.load(f)
+        
+def show_transitions():
+    while not q.empty():
+        while True:
+            try:
+                next_two = here.next()
+                print(next_two[0].filename, '->', next_two[1].filename)
+            except StopIteration:
+                print("All done.")
+                return
+                
 def trim_track(track, trim_start=0, trim_end=0):
 	"""Get echonest analysis object with count and return trimmed AudioQuantum list."""
 	music = audio.AudioQuantumList()
@@ -34,13 +100,6 @@ def trim_track(track, trim_start=0, trim_end=0):
 def tatum_count(track):
 	"""Get echonest analysis object and return tatum count."""
 	return len(track.analysis.tatums)
-
-def lazarus(filename):
-    """
-    Get pickled (*.en) filename (path) and return echonest analysis object
-    """
-    with open(filename) as f:
-        return pickle.load(f)
 
 def analize(input_filename):
     """
